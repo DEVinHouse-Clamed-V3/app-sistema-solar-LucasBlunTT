6f18c4dbc5c2000a5f5d2101c894c654905edbfd
Lucas da Silva <sklucassilva@gmail.com>
2024-09-13 15:14:39 -0300

    criacao dos cards dinamicos no container principal

--- utils/musicas.js
+++ utils/musicas.js
@@ -1,9 +1,27 @@
 import imageAlok from '../assets/artistas/alok.jpg';
+import vintage from '../assets/artistas/vintage.png';
+import brunobe from '../assets/artistas/bruno.png';
+import dubdogz from '../assets/artistas/subdog.png';
 
 export const musicas = [
   {
     imgAlbum: imageAlok,
     artista: 'The Weeknd',
-    duracao: '3:20', // duração da música em minutos e segundos
+    duracao: '5mi',
+  },
+  {
+    imgAlbum: vintage,
+    artista: 'Vintage Culture',
+    duracao: '8mi',
+  },
+  {
+    imgAlbum: brunobe,
+    artista: 'Bruno Be',
+    duracao: '9mi',
+  },
+  {
+    imgAlbum: dubdogz,
+    artista: 'DUBDOGZ',
+    duracao: '2mi',
   },
 ];
