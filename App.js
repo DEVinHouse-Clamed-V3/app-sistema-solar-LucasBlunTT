6523a069183a1b56dbb178a3e1997bd13d73afce
Lucas da Silva <sklucassilva@gmail.com>
2024-09-13 13:19:45 -0300

    Refatorar código para adicionar cabeçalho com imagem e título em App.js e atualizar cores em colors.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import { colors } from './styles/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View>
-        <Image/> <Text></Text>
+      <StatusBar style="auto" backgroundColor={colors.bakcground} />
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/83377646?v=4',
+          }}
+          style={{ width: 40, height: 40, borderRadius: 100, marginRight: 90 }}
+        />
+        <Text style={styles.titleHeader}>Músicas</Text>
       </View>
+      
     </View>
   );
 }
@@ -17,5 +23,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.bakcground,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+  },
+  titleHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    color: colors.title,
   },
 });
--- styles/colors.js
+++ styles/colors.js
@@ -2,4 +2,5 @@ export const colors = {
   bakcground: '#212121',
   button: '#212121',
   bgcard: '#535353',
+  title: '#fff',
 };
