64f1427710804cb43a7f38281acb393372d9e9ce
Lucas da Silva <sklucassilva@gmail.com>
2024-09-13 14:08:35 -0300

    criacao do card das musicas

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { colors } from './styles/colors';
 
 export default function App() {
@@ -14,13 +23,27 @@ export default function App() {
         />
         <Text style={styles.titleHeader}>Músicas</Text>
       </View>
-      
+      <ScrollView>
+        <View style={styles.card}>
+          <Image
+            source={require('./assets/alok.jpg')}
+            style={styles.cardImage}
+          />
+          <View style={styles.bottomCard}>
+            <Text style={styles.singer}>Alok</Text>
+            <TouchableOpacity>
+              <Text style={styles.follow}>Seguir</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 5,
     flex: 1,
     backgroundColor: colors.bakcground,
   },
@@ -34,5 +57,36 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 10,
     color: colors.title,
+  },
+  card: {
+    width: '100%',
+    marginBottom: 20,
+    paddingHorizontal: 40,
+  },
+  cardImage: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'contain',
+  },
+  bottomCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  singer: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.title,
+  },
+  follow: {
+    borderColor: '#CCC',
+    borderWidth: 1,
+    borderRadius: 50,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    color: colors.title,
+    fontSize: 15,
+    fontWeight: 'bold',
   },
 });
